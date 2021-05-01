c6f6cb2d244aef7195e5780b506ac29f4ebadb58
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-05-01 19:22:16 -0400

    adding comments

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
+// dependencies
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// const uuid = require("uuid");
-
+// config port
 const app = express();
 const PORT = process.env.PORT || 4000;
-
+// use 'public' folder content
 app.use(express.static('public'));
+// encrypt passed data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// get homepage
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
-
+// get notes page
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
-
+// get notes json
 app.get("/api/notes", (req, res) => {
     fs.readFile(__dirname + "/db/db.json", 'utf8', (err, data) => {
         if (err) {
@@ -23,7 +24,7 @@ app.get("/api/notes", (req, res) => {
         res.json(JSON.parse(data));
     });
 });
-
+// upload to notes
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     const noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -34,7 +35,7 @@ app.post("/api/notes", (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
     res.json(noteList);
 })
-
+// delete a note
 app.delete("/api/notes/:id", (req, res) => {
     let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let noteId = (req.params.id).toString();
@@ -44,13 +45,5 @@ app.delete("/api/notes/:id", (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
     res.json(noteList);
 })
-
-
-
-
-
-
-
-
-
+// run server
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
